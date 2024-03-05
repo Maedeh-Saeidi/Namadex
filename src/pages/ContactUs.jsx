6d864382288960a52a2b4a169e88388732986398
Maedeh <Maedeh@NGN.local>
2024-03-05 11:54:49 +0330

    Fixing ContactUs Component

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,23 +1,38 @@
 export default function ContactUs() {
   return (
-    <div className="h-screen bg-[url(../src/images/ContactUs.jpg)] bg-no-repeat bg-cover bg-contain flex flex-row items-center justify-end pr-[10rem] text-right">
-      <div className=" flex flex-col gap-[2.5rem] pt-[5rem]">
-        <h1 className="text-[40px] text-[#FFFFFF]">با ما در ارتباط باشید</h1>
-        <form className="flex flex-col gap-4  ">
-          <input
-            type="text"
-            className="bg-[#FFFFFF] rounded py-2 px-5 text-right"
-            placeholder="ایمیل خود را وارد کنید"
+    <div className="h-screen bg-no-repeat bg-cover flex flex-col justify-center ">
+      <div className="flex flex-row h-[30rem] bg-[url(../src/images/GreenBg.jpg)] ">
+        <div className="flex-1 flex flex-col justify-center items-center relative">
+          <img
+            src="../src/images/EmailP.png"
+            alt="Email"
+            width={500}
+            className="absolute bottom-[8rem]"
           />
-          <input
-            type="text"
-            className="bg-[#FFFFFF] rounded py-2 px-5 text-right w-[25rem]"
-            placeholder="متن پیام خود را بنویسید"
-          />
-          <button className="bg-[#FFFFFF] text-white py-2 px-5 rounded text-[#3EA98D]">
-            ارسال
-          </button>
-        </form>
+        </div>
+        <div className="flex-1 flex flex-col justify-center pb-[5rem] gap-[2.5rem] text-right">
+          <h1 className="text-[40px] text-[#FFFFFF] pr-[5rem]">
+            با ما در ارتباط باشید
+          </h1>
+          <form
+            action=""
+            className="flex flex-col gap-4 items-center pl-[5rem]"
+          >
+            <input
+              type="text"
+              className="w-[25rem] rounded py-2 px-5 text-right"
+              placeholder="ایمیل خود را وارد کنید"
+            />
+            <input
+              type="text"
+              className="w-[25rem] rounded py-2 px-5 text-right"
+              placeholder="متن پیام خود را بنویسید"
+            />
+            <button className="bg-[#FFFFFF] text-[#3EA98D]] py-2 px-5 rounded text-[#3EA98D] w-[25rem] text-center">
+              ارسال
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
