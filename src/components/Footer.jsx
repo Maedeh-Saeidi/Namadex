37a7f845fec43edb1668b1cb033e6b7ac45a6b45
Maedeh-Saeidi <saeidimdhw@gmail.com>
2024-07-08 16:38:19 +0330

    Adding Enamad

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,30 +5,46 @@ import FooterElements from "./FooterElements";
 
 export default function Footer() {
   return (
-    <div className=" gap-0 h-[18rem] sm:h-[35rem] bg-[#20292d] sm:bg-[url(../src/images/FooterBI.jpg)] bg-cover bg-no-repeat bg-contain flex flex-col sm:flex-row text-[#FFFFFF] xl:h-[35rem] sm:gap-0">
+    <div className="gap-0 h-[18rem] sm:h-[35rem] bg-[#20292d] sm:bg-[url(../src/images/FooterBI.jpg)] bg-cover bg-no-repeat flex flex-col sm:flex-row text-[#FFFFFF] xl:h-[35rem] sm:gap-0">
       <div className="hidden sm:flex-1 sm:flex justify-center items-center sm:mt-[10rem]">
         <div className="flex flex-col gap-1 mt-[5rem] text-[20px]">
           <motion.a
+            className="cursor:pointer"
             whileHover={{
               scale: 1.1,
               textShadow: "0px 0px 8px rgb(255,255,255)",
             }}
-            href="tel:982188600820+"
+            target="_blank"
+            href="https://trustseal.enamad.ir/?id=505639&Code=szzIQkre9wvQVg03GbpZe4o3hXIzV7Fs"
           >
-            +982188600820
+            <img
+              src="https://trustseal.enamad.ir/logo.aspx?id=505639&Code=szzIQkre9wvQVg03GbpZe4o3hXIzV7Fs"
+              alt="Enamad"
+              referrerpolicy="origin"
+              code="szzIQkre9wvQVg03GbpZe4o3hXIzV7Fs"
+            />
           </motion.a>
           <motion.a
             whileHover={{
               scale: 1.1,
               textShadow: "0px 0px 8px rgb(255,255,255)",
             }}
-            href="mailto:Namadex.ir@gmail.com"
+            href="tel:02188424516"
           >
-            Namadex.ir@gmail.com
+            02188424516
+          </motion.a>
+          <motion.a
+            whileHover={{
+              scale: 1.1,
+              textShadow: "0px 0px 8px rgb(255,255,255)",
+            }}
+            href="mailto:info@namadex.ir"
+          >
+            info@namadex.ir
           </motion.a>
         </div>
       </div>
-      <div className="mt-5 flex-1 flex flex-col gap-10 items-center justify-center sm:mt-[10rem]">
+      <div className="mt-5 flex-1 flex flex-col gap-1 items-center justify-center sm:mt-[10rem]">
         <a
           href=""
           className="hidden sm:flex flex-col items-center gap-2 2xl:pt-10 2xl:gap-0"
@@ -47,20 +63,40 @@ export default function Footer() {
           <SocialMedia color={"#FFFFFF"}></SocialMedia>
         </div>
       </div>
-      <div className="flex-1 sm:mt-[10rem] text-[14px] sm:text-[20px] mt-5 sm:mt-0">
+      <div className="flex-1 sm:mt-[11rem] 2xl:mt-[12.5rem] text-[14px] sm:text-[20px] mt-5">
         <ul className="flex flex-row gap-5 sm:flex-col items-center text-[12.5px] sm:text-[15px] justify-center sm:mt-[7.5rem] sm:gap-5">
           <FooterElements />
         </ul>
       </div>
       <div className="sm:hidden flex-1 flex justify-center items-center mb-5 ml-5">
-        <div className="flex flex-col gap-1 mb-[1rem] text-[15px]">
-          <a href="https://namadex.ir/" target="_blank" className="pl-7">
-            <img src={namadSvg} alt="" width={100} />
+        <div dir="rtl" className="flex flex-col gap-1 mb-[1rem] text-[15px]">
+          <div className="flex flex-row">
+            <a
+              href="https://namadex.ir/"
+              target="_blank"
+              className="pr-2 cursor-pointer"
+            >
+              <img src={namadSvg} alt="" width={100} />
+            </a>
+            <a
+              className="cursor-pointer pt-3"
+              referrerpolicy="origin"
+              target="_blank"
+              href="https://trustseal.enamad.ir/?id=505639&Code=szzIQkre9wvQVg03GbpZe4o3hXIzV7Fs"
+            >
+              <img
+                referrerpolicy="origin"
+                src="https://trustseal.enamad.ir/logo.aspx?id=505639&Code=szzIQkre9wvQVg03GbpZe4o3hXIzV7Fs"
+                alt="Enamad"
+                code="szzIQkre9wvQVg03GbpZe4o3hXIzV7Fs"
+              />
+            </a>
+          </div>
+
+          <a href="tel:02188424516" className="text-center mt-2">
+            02188424516
           </a>
-          <a href="tel:982188600820+" className="text-center mt-2">
-            982188600820+
-          </a>
-          <a href="mailto:Namadex.ir@gmail.com">Namadex.ir@gmail.com</a>
+          <a href="mailto:info@namadex.ir">info@namadex.ir</a>
         </div>
       </div>
     </div>
