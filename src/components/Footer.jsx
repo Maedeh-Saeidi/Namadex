38c7cf36f72de758e415cff8c1925b14f5a23113
Maedeh-Saeidi <saeidimdhw@gmail.com>
2024-07-08 17:19:44 +0330

    Fixing Enamad in Phone screens

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
   return (
     <div className="gap-0 h-[18rem] sm:h-[35rem] bg-[#20292d] sm:bg-[url(../src/images/FooterBI.jpg)] bg-cover bg-no-repeat flex flex-col sm:flex-row text-[#FFFFFF] xl:h-[35rem] sm:gap-0">
       <div className="hidden sm:flex-1 sm:flex justify-center items-center sm:mt-[10rem]">
-        <div className="flex flex-col gap-1 mt-[5rem] text-[20px]">
+        <div className="flex flex-col gap-1 mt-[10rem] text-[20px]">
           <motion.a
             className="cursor:pointer"
             whileHover={{
@@ -70,21 +70,22 @@ export default function Footer() {
       </div>
       <div className="sm:hidden flex-1 flex justify-center items-center mb-5 ml-5">
         <div dir="rtl" className="flex flex-col gap-1 mb-[1rem] text-[15px]">
-          <div className="flex flex-row">
+          <div className="flex flex-row items-baseline">
             <a
               href="https://namadex.ir/"
               target="_blank"
-              className="pr-2 cursor-pointer"
+              className="cursor-pointer"
             >
-              <img src={namadSvg} alt="" width={100} />
+              <img src={namadSvg} alt="Namadex" width={120} className="pt-1" />
             </a>
             <a
-              className="cursor-pointer pt-3"
+              className="cursor-pointer"
               referrerpolicy="origin"
               target="_blank"
               href="https://trustseal.enamad.ir/?id=505639&Code=szzIQkre9wvQVg03GbpZe4o3hXIzV7Fs"
             >
               <img
+                width={80}
                 referrerpolicy="origin"
                 src="https://trustseal.enamad.ir/logo.aspx?id=505639&Code=szzIQkre9wvQVg03GbpZe4o3hXIzV7Fs"
                 alt="Enamad"
@@ -92,11 +93,12 @@ export default function Footer() {
               />
             </a>
           </div>
-
-          <a href="tel:02188424516" className="text-center mt-2">
-            02188424516
-          </a>
-          <a href="mailto:info@namadex.ir">info@namadex.ir</a>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <a href="tel:02188424516" className="text-center mt-2">
+              02188424516
+            </a>
+            <a href="mailto:info@namadex.ir">info@namadex.ir</a>
+          </div>
         </div>
       </div>
     </div>
